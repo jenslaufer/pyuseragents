fb5e3879b39ee927712792641a3258834883386e
Jens Laufer <jenslaufer@gmail.com>
2019-02-19 14:18:01 +0100

    inital implememtation

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='amznscrp',
+    url='https://github.com/jenslaufer/pyuseragents',
+    author='Jens Laufer',
+    author_email='jenslaufer@gmail.com',
+    packages=['pyuseragent'],
+    install_requires=[],
+    version='0.1.0',
+    license='MIT',
+    description='module to get randomly a useragent for scraping',
+    include_package_data=True
+)
