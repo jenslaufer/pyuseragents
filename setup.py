1f3d1b88839d2690f8f1de62e29761eef430e02d
Jens Laufer <jenslaufer@gmail.com>
2019-02-19 14:20:27 +0100

    fixed module name

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='amznscrp',
+    name='pyuseragents',
     url='https://github.com/jenslaufer/pyuseragents',
     author='Jens Laufer',
     author_email='jenslaufer@gmail.com',
